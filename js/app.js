972f9d594132877c8f723d029beacb7873cfe9a6
loChris <44450518+loChris@users.noreply.github.com>
2019-07-17 20:23:15 -0500

    Refactored: with event delegation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
+$('.spoiler').on('click', 'button', () => {
+    $('.spoiler span').show();
+    $('.spoiler button').hide();
+});
+
 // create reveal spoiler button
 const $button = $('<button>Reveal Spoiler</button>');
+
 // append to web page
 $('.spoiler').append($button);
 
 // hide the spoiler text
 $('.spoiler span').hide();
-$('.spoiler button').click(() => {
-    $('.spoiler span').show();
-    $('.spoiler button').hide();
-});
